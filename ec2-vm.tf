7ad110e871eb1c7036a1a98507356b277b80a403
Sharma Ankit (BD/DIA-GOI1) <Ankit.Sharma@bosch.com>
2024-03-04 20:15:39 +0530

    adding separte files

---
+++ ec2-vm.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "project-iac" {
+  ami = var.ami
+  instance_type = var.itype
+  subnet_id = var.subnet #FFXsubnet2
+  associate_public_ip_address = var.publicip
+  key_name = var.key_name
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 50
+    volume_type = "gp3"
+  }
+  tags = {
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
