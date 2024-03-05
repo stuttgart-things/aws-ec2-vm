6569a1dc966b2303e6fff602493c081ff787d115
Sharma Ankit (BD/DIA-GOI1) <Ankit.Sharma@bosch.com>
2024-03-05 10:06:31 +0530

    added variable type

--- variables.tf
+++ variables.tf
@@ -1,44 +1,53 @@
 variable "region" {
   default     = false
+  type        = string
   description = "AWS region"
 }
 
 variable "vpc" {
   default     = false
+  type        = string
   description = "Virtual Private Cloud id"
 }
 
 variable "ami" {
   default     = false
+  type        = string
   description = "AWS Machine Image id"
 }
 
 variable "itype" {
   default     = false
+  type        = string
   description = "Instance Type"
 }
 
 variable "subnet" {
   default     = false
+  type        = string
   description = "Subnet id"
 }
 
 variable "publicip" {
   default     = false
+  type        = string
   description = "Use of public ip True/false"
 }
 
 variable "key_name" {
   default     = false
+  type        = string
   description = "Key name for ssh key"
 }
 
 variable "secgroupname" {
   default     = false
+  type        = string
   description = "Name of Security Group"
 }
 
 variable "ssh_path" {
   default     = false
+  type        = string
   description = "Path for ssh public key"
 }
