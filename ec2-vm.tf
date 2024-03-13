ab2229c0cffa8a6e0320190415ec06a2a7cbb57a
ankitsharma12-ops <ankit.saggitarius01@gmail.com>
2024-03-13 12:02:55 +0530

    added data block

--- ec2-vm.tf
+++ ec2-vm.tf
@@ -30,3 +30,12 @@ resource "aws_instance" "project-iac" {
 
   depends_on = [aws_security_group.project-iac-sg]
 }
+
+data "template_file" "clout-init.yaml" {
+  template = file("${path.module}/templates/cloud-init.yaml.tpl")
+  vars = {
+    package_update  = var.package_update
+    package_upgrade = var.package_upgrade
+  }
+}
+
