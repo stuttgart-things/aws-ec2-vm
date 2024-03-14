ff41c94711a66376afbccea689d08a072ba34e05
Patrick Hermann <patrick.hermann@sva.de>
2024-03-14 07:11:00 +0000

    updated templaing for packages

--- tests/create-module-test.py
+++ tests/create-module-test.py
@@ -3,9 +3,14 @@ import yaml
 import random
 from jinja2 import Template
 import os
+import argparse
 
 # DIRECTORY CREATION & CHECK IF IT EXISTS OR NOT
 path = "/tmp/aws-ec2-vm"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--values', default='values.yaml')
+args = parser.parse_args()
 
 if not os.path.exists(path):
   os.mkdir(path)
@@ -35,7 +40,7 @@ def render_template(values):
 def main():
 
   # LOAD YAML FILE
-  with open('values.yaml', 'r') as f:
+  with open(args.values, 'r') as f:
       values = yaml.load(f, Loader=yaml.SafeLoader)
 
   # ITERATE OVER THE VALUES DICTIONARY + GET RANDOM VALUE
